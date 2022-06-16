4453e351d0d4180d5d715db2f4b6aae015047187
carlosjrw <ccpjunior2013@gmail.com>
2022-06-16 03:09:30 -0400

    Tratando excessoes no cadastro de email repetido

--- src/main/java/com/castro/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/castro/api/exceptionhandler/ApiExceptionHandler.java
@@ -15,8 +15,12 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.castro.api.domain.exception.NegocioException;
+
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
@@ -46,6 +50,18 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		problema.setCampos(campos);
 
 		return handleExceptionInternal(ex, problema, headers, status, request);
+	}
+	
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<Object> hendleNegocio(NegocioException ex, WebRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+
+		Problema problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setDataHora(LocalDateTime.now());
+		problema.setTitulo(ex.getMessage());
+
+		return handleExceptionInternal(ex, problema, new HttpHeaders(), status, request);
 	}
 
 }
