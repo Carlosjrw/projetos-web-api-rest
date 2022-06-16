a70114269a844387d11fa1e8ac94a9c3ff62c278
carlosjrw <ccpjunior2013@gmail.com>
2022-06-16 01:14:17 -0400

    Projeto criado

---
+++ src/main/java/com/castro/api/ProjetosWebApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.castro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetosWebApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetosWebApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/castro/api/ProjetosWebApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castro.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetosWebApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
